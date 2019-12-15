c51644c9da510abc4ddfe808a8e1458449ebfb95
Joshua Patterson <jleegippies@gmail.com>
2019-12-14 21:48:14 -0600

    Added switch

--- plumbing.py
+++ plumbing.py
@@ -5,3 +5,9 @@ def selector(s, a_1, a_0):
     a_1_and = gate_and(s, a_1)
     a_0_and = gate_and(gate_not(s), a_0)
     return gate_or(a_1_and, a_0_and)
+
+
+def switch(s, a):
+    c_1 = gate_and(s, a)
+    c_0 = gate_and(gate_not(s), a)
+    return c_1, c_0
