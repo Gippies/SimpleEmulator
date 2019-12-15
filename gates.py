b68d7772b70295f3971ee8c81351ce75d69f8c74
Joshua Patterson <jleegippies@gmail.com>
2019-12-14 18:14:48 -0600

    Added some basic logic gates

--- gates.py
+++ gates.py
@@ -1,2 +1,14 @@
-def nand(a, b):
+def gate_nand(a, b):
     return not (a and b)
+
+
+def gate_and(a, b):
+    return a and b
+
+
+def gate_or(a, b):
+    return a or b
+
+
+def gate_xor(a, b):
+    return a ^ b
