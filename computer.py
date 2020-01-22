836baf4babc67bbdecfb8f759bbce0956b8516d6
Joshua Patterson <jlpatter@ksu.edu>
2020-01-22 16:31:52 -0600

    Fixed issue where emulator would crash when the program was finished instead of exiting gracefully

--- computer.py
+++ computer.py
@@ -7,23 +7,28 @@ class Computer:
         self.control_unit = ControlUnit()
         self.program = []
         self.program_current_address = 0
+        self.is_running = True
 
     def _run_program_on_control_unit(self, clock):
         if self.program_current_address >= len(self.program) or self.program_current_address < 0:
-            raise ValueError(f"Error, tried to access program address {self.program_current_address} which doesn't exist")
-        j, a_register_value = self.control_unit.do_control_unit(self.program[self.program_current_address], clock)
-        if j == 1 and clock == 1:
-            self.program_current_address = convert_bit_list_to_num(a_register_value)
-        elif clock == 1:
-            self.program_current_address += 1
-        print(f"A register value: {a_register_value}")
-        print(f"Current program address: {self.program_current_address}")
+            print(f"Reached program address {self.program_current_address} which is outside the allocated program (from 0 to {len(self.program) - 1} inclusive)")
+            print(f"Shutting Down Emulator...")
+            self.is_running = False
+        else:
+            j, a_register_value = self.control_unit.do_control_unit(self.program[self.program_current_address], clock)
+            if j == 1 and clock == 1:
+                self.program_current_address = convert_bit_list_to_num(a_register_value)
+            elif clock == 1:
+                self.program_current_address += 1
+            print(f"Register A value: {a_register_value}")
+            print(f"Current program address: {self.program_current_address}")
 
     def do_computer(self):
         # Performs one clock cycle
         if len(self.program) == 0:
             raise ValueError("Error, tried to run the computer without a program loaded")
-        while True:
+        print("Executing program...")
+        while self.is_running:
             self._run_program_on_control_unit(1)
             self._run_program_on_control_unit(0)
 
@@ -36,4 +41,4 @@ class Computer:
                     if c != '\n':
                         instruction_bit_list.append(int(c))
                 self.program.append(instruction_bit_list)
-        print("Program Loaded, Executing...")
+        print("Program loaded")
